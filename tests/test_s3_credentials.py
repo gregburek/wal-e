e18404b8a5bcf55ae753196477b8065eb8a8333d
Daniel Farina <daniel@fdr.io>
2013-05-29 12:15:41 -0700

    wip
    Signed-off-by: Daniel Farina <daniel@fdr.io>

--- tests/test_s3_credentials.py
+++ tests/test_s3_credentials.py
@@ -1,7 +1,8 @@
 import pytest
 
-from wal_e.s3 import credentials
 from wal_e import exception
+from wal_e import s3
+from wal_e.s3 import credentials
 
 
 @pytest.fixture(autouse=True)
@@ -10,6 +11,49 @@ def never_use_aws_env_vars(monkeypatch):
 
     for name in ['AWS_ACCESS_KEY_ID', 'AWS_SECRET_ACCESS_KEY']:
         monkeypatch.delenv(name, raising=False)
+
+
+@pytest.fixture
+def instance_metadata_iam(monkeypatch):
+    """Return generic instance metadata with IAM keys."""
+    from boto import utils
+
+
+    # Fixture for get_instance_metadata, copied from the boto test
+    # suite.
+    INSTANCE_CONFIG = {
+        'allowall': {
+            u'AccessKeyId': u'iam_access_key',
+            u'Code': u'Success',
+            u'Expiration': u'2012-09-01T03:57:34Z',
+            u'LastUpdated': u'2012-08-31T21:43:40Z',
+            u'SecretAccessKey': u'iam_secret_key',
+            u'Token': u'iam_token',
+            u'Type': u'AWS-HMAC'
+            }
+        }
+
+    monkeypatch.setattr(utils, 'get_instance_metadata',
+                        lambda: INSTANCE_CONFIG)
+
+
+@pytest.fixture
+def instance_metadata_no_iam(monkeypatch):
+    """Return generic instance metadata without IAM keys."""
+    from boto import utils
+
+    INSTANCE_CONFIG = {
+        'allowall': {
+            u'Code': u'Success',
+            u'Expiration': u'2012-09-01T03:57:34Z',
+            u'LastUpdated': u'2012-08-31T21:43:40Z',
+            u'Token': u'iam_token',
+            u'Type': u'AWS-HMAC'
+            }
+        }
+
+    monkeypatch.setattr(utils, 'get_instance_metadata',
+                        lambda: INSTANCE_CONFIG)
 
 
 def test_env_complete(monkeypatch):
@@ -81,31 +125,8 @@ def test_env_instance_profile(monkeypatch):
         cred.raise_on_incomplete()
 
 
-def test_argv_complete():
-    """Tests creds fully specified via argv."""
-    cred = credentials.from_argv(public='PUBLIC-BOGUS',
-                                 private='PRIVATE-BOGUS')
-    assert cred.public_source is credentials.Argv
-    assert cred.private_source is credentials.Argv
-    assert cred.is_complete
-    assert cred.public == 'PUBLIC-BOGUS'
-    assert cred.private == 'PRIVATE-BOGUS'
-    cred.raise_on_incomplete()
-
-
-def test_argv_empty():
-    """Tests creds not specified at all."""
-    cred = credentials.from_argv()
-    assert not cred.is_complete
-    assert cred.public_source is credentials.Argv
-    assert cred.private_source is credentials.Argv
-
-    with pytest.raises(credentials.IncompleteCredentials):
-        cred.raise_on_incomplete()
-
-
-def test_argv_no_private():
-    """Tests missing private creds."""
+def test_argv_public_only():
+    """Tests argv key public input with missing private creds."""
     cred = credentials.from_argv(public='PUBLIC-BOGUS')
 
     assert cred.public_source is credentials.Argv
@@ -113,22 +134,6 @@ def test_argv_no_private():
 
     assert cred.private is None
     assert cred.public == 'PUBLIC-BOGUS'
-
-    assert not cred.is_complete
-
-    with pytest.raises(credentials.IncompleteCredentials):
-        cred.raise_on_incomplete()
-
-
-def test_argv_no_public():
-    """Tests missing public creds."""
-    cred = credentials.from_argv(private='PRIVATE-BOGUS')
-
-    assert cred.public_source is credentials.Argv
-    assert cred.private_source is credentials.Argv
-
-    assert cred.private == 'PRIVATE-BOGUS'
-    assert cred.public is None
 
     assert not cred.is_complete
 
@@ -144,3 +149,65 @@ def test_argv_instance_profile():
 
     with pytest.raises(exception.UserCritical):
         cred.raise_on_incomplete()
+
+
+def test_search_simple_no_argv():
+    """Tests credential search that doesn't rely on argv."""
+    cred = credentials.search_credentials(None)
+
+    assert not cred.is_complete
+    assert cred.public_source is credentials.Environ
+    assert cred.private_source is credentials.Environ
+
+    with pytest.raises(credentials.IncompleteCredentials):
+        cred.raise_on_incomplete()
+
+
+def test_search_nothing_specified():
+    cred = s3.search_credentials(None)
+
+    assert not cred.is_complete
+
+    with pytest.raises(credentials.IncompleteCredentials):
+        cred.raise_on_incomplete()
+
+
+def test_search_full_env(monkeypatch):
+    monkeypatch.setenv('AWS_ACCESS_KEY_ID', 'PUBLIC-BOGUS')
+    monkeypatch.setenv('AWS_SECRET_ACCESS_KEY', 'PRIVATE-BOGUS')
+
+    cred = s3.search_credentials(None)
+
+    assert cred.is_complete
+    cred.raise_on_incomplete()
+
+
+def test_search_okay_blend(monkeypatch):
+    monkeypatch.setenv('AWS_SECRET_ACCESS_KEY', 'PRIVATE-BOGUS')
+
+    cred = s3.search_credentials('PUBLIC-BOGUS')
+
+    assert cred.is_complete
+    cred.raise_on_incomplete()
+
+
+def test_search_busted(monkeypatch):
+    monkeypatch.setenv('AWS_ACCESS_KEY_ID', 'PUBLIC-BOGUS')
+
+    cred = s3.search_credentials('PUBLIC-BOGUS')
+
+    assert not cred.is_complete
+
+    with pytest.raises(credentials.IncompleteCredentials):
+        cred.raise_on_incomplete()
+
+
+def test_search_env_triggered_expansion(monkeypatch, instance_metadata_iam):
+    monkeypatch.setenv('AWS_ACCESS_KEY_ID', 'instance-profile')
+
+    cred = s3.search_credentials(None)
+
+    print cred.public
+    print cred.private
+    assert cred.is_complete
+    cred.raise_on_incomplete()
