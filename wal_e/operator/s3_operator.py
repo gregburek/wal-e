61e07f6be19f56f26547e4d7533ad9e9ca0ee450
Daniel Farina <daniel@fdr.io>
2013-07-26 02:01:43 -0700

    Always use prefetch path

--- wal_e/operator/s3_operator.py
+++ wal_e/operator/s3_operator.py
@@ -387,11 +387,8 @@ class S3Backup(object):
         basename(wal_path), so both are required.
         """
         xlog_dir = path.dirname(wal_destination)
-        pd = None
-
-        if concurrency > 1:
-            pd = PrefetchDirs(xlog_dir)
-            pd.create()
+        pd = PrefetchDirs(xlog_dir)
+        pd.create()
 
         segment = worker.WalSegment(wal_name, explicit=True)
 
